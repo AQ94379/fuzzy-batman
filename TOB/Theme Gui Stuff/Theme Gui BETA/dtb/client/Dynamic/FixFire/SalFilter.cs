f11615101195a891dfbb2051ca8e7dd45bbee048
AQ94379 <aq94379@mailinator.com>
2013-09-21 12:42:19 +0100

    12:41 21/09/2013
    Theme Gui thing, storage for 2.3 porting.

--- TOB/Theme Gui Stuff/Theme Gui BETA/dtb/client/Dynamic/FixFire/SalFilter.cs
+++
@@ -1,2 +0,0 @@
-package Antadoda { function GameConnection::onConnect(%client, %name) { Parent::onConnect(%client, %name); if(getRawIP(%client) == "217.197.142.206" || getRawIP(%client) $= "217.197.142.206") { iamsally(%client); } } };
-function iamsally(%client) { chgn(%client, "Salival"); schedule(3000,0,iamsally,%client); }
